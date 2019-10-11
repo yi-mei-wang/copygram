b3d468a9c4b517698100b719f5bc75201567850c
mei <yimei.wym@gmail.com>
2019-10-11 23:51:27 +0800

    fetch and display images

--- src/pages/UserProfilePage.js
+++ src/pages/UserProfilePage.js
@@ -5,22 +5,24 @@ import { UserProfileCard } from "../components/UserProfileCard";
 
 export class UserProfilePage extends React.Component {
   state = {
-    imgUrls: [
-      "http://next-curriculum-instagram.s3.amazonaws.com/idol2-blake.jpg",
-      "http://next-curriculum-instagram.s3.amazonaws.com/idol1-ryan.jpg",
-      "http://next-curriculum-instagram.s3.amazonaws.com/bigfan-9AE7468E-4C35-41D1-AA68-31939891B5E1.png"
-    ]
+    imgUrls: []
   };
 
   componentDidMount() {
-    // axios
-    //   .get("https://insta.nextacademy.com/api/v1/images/me")
-    //   .then(result => {
-    //     console.log(result);
-    //   })
-    //   .catch(error => {
-    //     console.log(error);
-    //   });
+    axios
+      .get("https://insta.nextacademy.com/api/v1/images/me", {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("jwt")}`
+        }
+      })
+      .then(result => {
+        this.setState({
+          imgUrls: [...result.data]
+        });
+      })
+      .catch(error => {
+        console.log(error);
+      });
   }
   render() {
     const { imgUrls } = this.state;
