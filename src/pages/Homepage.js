112895cb9e01b195936c0a065bf2887efa836c3a
mei <yimei.wym@gmail.com>
2019-10-15 10:52:31 +0800

    make call inside homepage

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,7 +1,19 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { RoundImage as ProfileImage } from "../styled/RoundImage";
+import { getData } from "../helpers/APICalls";
 
-export const Homepage = ({ users }) => {
+export const Homepage = () => {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      const resp = await getData("/users");
+      setUsers(resp.data);
+    };
+
+    fetchUsers();
+  }, []);
+
   return (
     <ul>
       {users.map((user, index) => (
