20294a548864c05257fecf5b6d4491320b0f9b18
mei <yimei.wym@gmail.com>
2019-10-16 22:06:55 +0800

    vertically centre upload form

--- src/pages/UploadPage.js
+++ src/pages/UploadPage.js
@@ -58,8 +58,10 @@ export default class UploadPage extends React.Component {
       <>
         <div
           style={{
+            height: "70vh",
             display: "flex",
             alignItems: "center",
+            justifyContent: "center",
             flexDirection: "column"
           }}
         >
